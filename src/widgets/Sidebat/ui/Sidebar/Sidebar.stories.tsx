e1c0abbd07a217b0fad54d754c17997b17b37e74
Grigo471 <grigo471@gmail.com>
2023-05-22 04:13:18 +0300

    fixed sidebar stories

--- src/widgets/Sidebat/ui/Sidebar/Sidebar.stories.tsx
+++ src/widgets/Sidebat/ui/Sidebar/Sidebar.stories.tsx
@@ -2,6 +2,7 @@ import type { Meta, StoryObj } from '@storybook/react';
 
 import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
 import { Theme } from 'app/providers/ThemeProvider';
+import { RouteDecorator } from 'shared/config/storybook/RouteDecorator/RouteDecorator';
 import { Sidebar } from './Sidebar';
 
 const meta: Meta<typeof Sidebar> = {
@@ -10,6 +11,7 @@ const meta: Meta<typeof Sidebar> = {
     tags: ['autodocs'],
     argTypes: {
     },
+    decorators: [RouteDecorator],
 };
 
 export default meta;
