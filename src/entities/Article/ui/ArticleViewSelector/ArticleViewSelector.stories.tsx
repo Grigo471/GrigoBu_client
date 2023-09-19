d8554409016e4365ebc3eb78bb59df59a5ed22f3
Grigo471 <grigo471@gmail.com>
2023-09-19 17:38:17 +0300

    add fetch articles, view type selector

---
+++ src/entities/Article/ui/ArticleViewSelector/ArticleViewSelector.stories.tsx
@@ -0,0 +1,31 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
+import { Theme } from 'app/providers/ThemeProvider';
+import { ArticleViewSelector } from './ArticleViewSelector';
+
+const meta: Meta<typeof ArticleViewSelector> = {
+    title: 'shared/ArticleViewSelector',
+    component: ArticleViewSelector,
+    tags: ['autodocs'],
+    argTypes: {
+    },
+};
+
+export default meta;
+type Story = StoryObj<typeof ArticleViewSelector>;
+
+export const Light: Story = {
+    args: {
+
+    },
+};
+
+export const Dark: Story = {
+    args: {
+
+    },
+    decorators: [
+        ThemeDecorator(Theme.DARK),
+    ],
+};
